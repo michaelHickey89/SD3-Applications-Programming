83bd7b8e4ca3a45e64fd7452f6cee04d1cb06779
lit-alan <Alan.Ryan@lit.ie>
2019-10-17 16:58:25 +0100

    Solution added for BookManager Lambda Exercise

---
+++ Source/Lab Exercises/Lambda Lab Exercise/Lab Ex 2 - BookCatalog/BookCatalogSoln/BookCatalogSoln/src/sd3/bookcatalog/src/BookManager.java
@@ -0,0 +1,22 @@
+package sd3.bookcatalog.src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+    private List<Book> bookList;
+    
+    public BookManager() {
+        bookList = new BookCatalog().getCatalog();
+    }
+    
+    public List<Book> getBooksByLambda(TestBook condition) {
+        List<Book> books = new ArrayList<>();
+        for (Book b: bookList) {
+            if (condition.test(b)) {
+                books.add(b);
+            }
+        }
+        return books;
+    }
+}
