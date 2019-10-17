299517ff31bc4f2e28b444e296d299603bb529dc
lit-alan <Alan.Ryan@lit.ie>
2019-10-17 09:53:39 +0100

    Updated Lambda Source Code

--- Source/L8/CollectionsGenericsLambdasSource/src/lambdas/basic/TestMathOperation.java
+++ Source/L8/CollectionsGenericsLambdasSource/src/lambdas/basic/TestMathOperation.java
@@ -21,35 +21,7 @@ public class TestMathOperation {
          MathOperation division  = (int a, int b) -> b / a;
          System.out.println("\n20 / 10  = " + operate(10, 20, division));
          
-         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-         
-         CountOperation countOdd   = (int i) -> {  return i % 2 != 0;};
-         CountOperation countEven  = (int i) -> {  return i % 2 != 0;};
-         CountOperation countSeven = (int i) -> {  return i == 7;};
-         CountOperation countPrime = (int i) -> {  
-             if (i%2==0) return false;
-                for(int j=3;j*j<=i;j+=2) {
-                    if(j%i==0)
-                        return false;
-                }
-                return true;
-         };
-       
-       
-        
-         
-        System.out.println("Number of odd numbers in " + Arrays.toString(arr) + countIt(arr, countOdd) );
-        System.out.println("Number of even numbers in " + Arrays.toString(arr) + countIt(arr, countEven) );
-        System.out.println("Number of 7's numbers in " + Arrays.toString(arr) + countIt(arr, countSeven) );
-        System.out.println("Number of prime numbers in " + Arrays.toString(arr) + countIt(arr, countPrime) );
-    
-        Integer[] arr2 = {1,7,7,4};
-        CountOperation1 countit = (Object o) -> { int i = (Integer)o; return i == 7;}; 
-        System.out.println("test  generic " +  countIt2(arr2, countit) );
-    
-        String[] arr3 = {"Alan", "Aoife", "Erin", "Rebel"};
-        CountOperation1 countit2 = (Object o) -> { String s = (String)o; return s.startsWith("A");}; 
-        System.out.println(countIt2(arr3, countit2));
+                     
     }//end method
     
     public int operate(int a, int b, MathOperation math) {
@@ -57,26 +29,5 @@ public class TestMathOperation {
         return math.operation(a, b);
     }//end method
     
-    
-    public int countIt(int arr[], CountOperation op) {
-        int count = 0;
-         for (int i : arr) {
-             if (op.test(i)) 
-                 count++;
-         }
-         
-         return count;
-        
-    }
-    public <T> int countIt2(T arr[], CountOperation1 op) {
-        int count = 0;
-         for (T i : arr) {
-             if (op.test(i)) 
-                 count++;
-         }
-         
-         return count;
-        
-    }
 
 }//end class
